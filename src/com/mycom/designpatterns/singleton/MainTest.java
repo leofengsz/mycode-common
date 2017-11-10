0b7b57bc9d9fba7a82efd9f37bad7e0b4a2aa42d
leofengsz <95351103@qq.com>
2017-11-10 10:34:17 +0800

    添加单例模式、简单工厂模式注释

--- src/com/mycom/designpatterns/singleton/MainTest.java
+++ src/com/mycom/designpatterns/singleton/MainTest.java
@@ -4,8 +4,17 @@ package com.mycom.designpatterns.singleton;
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		/**
+		 * 优点：
+		 * 1. 提供唯一实例，控制对其访问，实现共享概念。
+		 * 2. 节约资源，提高性能（针对频繁创建和销毁的对象）。
+		 * 3. 允许可变数目的实例。
+		 * 
+		 * 缺点：
+		 * 1. 没有抽象层，扩展性差。
+		 * 2. 职责过重。
+		 * 3. 由于JVM自动回收，可能导致状态丢失。
+		 */
 		SingletonLHS.getInstance().printtest();
 		SingletonEHS.getInstance().printtest();
 		
--- src/com/mycom/designpatterns/singleton/Test1.java
+++
@@ -1,9 +0,0 @@
-package com.mycom.designpatterns.singleton;
-
-public class Test1 {
-  
-  public static void main(String[] args) {
-    
-  }
-
-}
